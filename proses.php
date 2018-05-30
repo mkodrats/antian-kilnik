cd0d3e48226958bbd7fd8f4d5f82d7df9d11443a
Muhammad Kodrat <muhammadkodrat@icloud.com>
2018-05-30 21:50:22 +0700

    initial commit

--- proses.php
+++ proses.php
@@ -3,8 +3,8 @@
     function Connection()
     {
         static $conn;
-        // $conn = mysqli_connect('localhost','root','root','simrsst');
-        $conn = mysqli_connect('den1.mysql2.gear.host','simrsst','Ca80ec!~R32L','simrsst');
+        $conn = mysqli_connect('localhost','root','root','simrsst');
+        // $conn = mysqli_connect('den1.mysql2.gear.host','simrsst','Ca80ec!~R32L','simrsst');
         return $conn;
     }
 
@@ -34,24 +34,29 @@
 
     function Daftar($norm, $tgl_lahir, $password)
     {
-        $conn = Connection();
-        $sql = "SELECT NomorRM, TglLahir FROM t_pasien WHERE NomorRM = '$norm' AND TglLahir = '$tgl_lahir' ";
+        $conn   = Connection();
+        $sql    = "SELECT NomorRM, TglLahir FROM t_pasien WHERE NomorRM = '$norm' AND TglLahir = '$tgl_lahir' ";
+        $res    = mysqli_query($conn, $sql);
+        $query  = "SELECT NomorRM FROM t_user WHERE NomorRM = '$norm'";
+        $result = mysqli_query($conn, $query);
+        $tes = mysqli_fetch_row($res);
+        // var_dump($tes);
 
-        $res = mysqli_query($conn, $sql);
-        if (mysqli_fetch_row($res) == 0) {
-           $alert = 'Pasien Belum Terdaftar';
-           echo json_encode($alert);
-        }
-        if (mysqli_fetch_row($res) > 0) {
-           
+        $alert = [];
+        if (mysqli_fetch_row($res) > 0 &&  mysqli_fetch_row($result) == 0) {
             $sql  = "INSERT INTO t_user(NomorRM, password ) VALUES ('$norm', '$password') ";
             $res = mysqli_query($conn, $sql);
             if ($res == true) {
-               $alert =  'Daftar Berhasil Silahkan Login';
+               $alert = array('alert' => 'Daftar Berhasil Silahkan Login');
                echo json_encode($alert);
             }
+        }else if (mysqli_fetch_row($result) > 0) {
+           $alert = array('alert' => 'Anda Sudah Terdaftar Silahkan Login');
+           echo json_encode($alert);
         }else{
-            $alert = 'Anda Sudah Terdaftar';
+            $alert = array(
+                'alert' => 'Terjadi Kesalahan'
+            );
             echo json_encode($alert);
         }
     }
